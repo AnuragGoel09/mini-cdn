2d79494cb14520f232fd870bf703ef8933766aef
AnuragGoel09 <anurag.goel.9303@gmail.com>
2026-08-02 16:24:54 +0530

    updated render file

--- router-service/src/main/java/com/minicdn/router/RouterServiceApplication.java
+++ router-service/src/main/java/com/minicdn/router/RouterServiceApplication.java
@@ -21,8 +21,8 @@ public class RouterServiceApplication {
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
         return builder
-                .setConnectTimeout(Duration.ofSeconds(3))
-                .setReadTimeout(Duration.ofSeconds(5))
+                .setConnectTimeout(Duration.ofSeconds(10))
+                .setReadTimeout(Duration.ofSeconds(70))
                 .build();
     }
 }
