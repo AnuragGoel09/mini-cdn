d4a3e6d79acd2e44e9f897e86e2e81e65dd83498
AnuragGoel09 <anurag.goel.9303@gmail.com>
2026-08-02 17:03:27 +0530

    updated render file

--- router-service/src/main/java/com/minicdn/router/WebConfig.java
+++ router-service/src/main/java/com/minicdn/router/WebConfig.java
@@ -8,6 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST");
+        registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST")
+        .exposedHeaders("X-Cache", "X-Cache-Node", "X-Router-Latency-Ms", "X-Client-Region-Guess");
     }
 }
